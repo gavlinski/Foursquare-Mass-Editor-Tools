d752000ae584edcb60f3d273a91f9d95f19370bf
Elio <elio@elio-desktop.(none)>
2012-02-01 16:17:20 -0200

    Adicionada importação de página salva do Places. Alterados alguns campos default. Excluído arquivo edit_venue.php (movido para a pasta backups).

--- edit_csv.php
+++ edit_csv.php
@@ -1,6 +1,6 @@
 <?php
 mb_internal_encoding("UTF-8");
-mb_http_output( "iso-8859-1" );
+mb_http_output("iso-8859-1");
 ob_start("mb_output_handler");
 header("Content-Type: text/html; charset=ISO-8859-1",true);
 
