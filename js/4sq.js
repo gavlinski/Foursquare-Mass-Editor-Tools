e56e0717c8030c6742fcc8dea90bd5a5d40e5abc
Elio Gavlinski <gavlinski@gmail.com>
2026-02-22 22:28:53 -0300

    chore: Atualizar DATA_VERSIONAMENTO da API para 20250401
    **Atualização de versionamento:**
    - Atualizado de 20230404 (abril/2023) para 20250401 (abril/2025)
    - Arquivos: js/4sq_csv.js e js/4sq.js
    
    **Motivação:**
    - Versão anterior estava desatualizada há ~3 anos
    - Nova versão inclui melhorias de abril/2024 (Category Codes)
    - Testado com test_api_version.html - nenhuma mudança estrutural detectada
    - Segue recomendações da documentação oficial do Foursquare
    
    **Benefícios:**
    - Suporte a Category Codes além de Category IDs (abril/2024)
    - API mais atualizada e otimizada
    - Melhor compatibilidade futura
    - Mantém estabilidade (não usa data atual)
    
    **Referência:**
    - Docs: https://docs.foursquare.com/developer/reference/versioning

--- js/4sq.js
+++ js/4sq.js
@@ -11,7 +11,7 @@ dojo.require("dijit.Menu");
 dojo.require("dojo.cookie");
 dojo.require("dijit.form.Select");
 
-var DATA_VERSIONAMENTO = "20230404";
+var DATA_VERSIONAMENTO = "20250401";
 var MESES = new Array("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12");
 
 var modo;
